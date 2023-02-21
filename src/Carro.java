7657e740090f59e8effcf9e047a6bc736162541f
Ricardocgon <ricardocgon@gmail.com>
2023-02-21 19:24:04 -0300

    Atualiza

--- src/Carro.java
+++ src/Carro.java
@@ -3,5 +3,44 @@ public class Carro {
         String cor;
         String modelo;
         int capacidadeTanque;
+
+    public Carro() {
+    }
+
+    //Sobrecarga de Método
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    double totalValorTanque (double valorCombustivel){
+        return capacidadeTanque * valorCombustivel;
+
     }
 }
+
